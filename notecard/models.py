4c5c010fc9285b3a76a77047d6bc8db251170dbd
Jake Gaylor <jhgaylor@gmail.com>
2013-04-20 00:10:57 -0500

    model

--- notecard/models.py
+++ notecard/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Notecard(models.Model):
+    front = models.TextField()
+    back = models.TextField()
+    name = models.CharField(max_length=100, required=True)
+    learned = models.BooleanField(default=False)
+    times_reviewed = models.IntegerField(default=0)
+    times_right = models.IntegerField(default=0)
+
+    # def Review(self):
+    #   self.times_reviewed += 1
+    #   self.save()
+
+    # def mark_right(self):
+    #   self.times_right += 1
+    #   self.save()
+
+    #def get_absolute_url(self):
+
+    def __unicode__(self):
+        return self.name 
+
+
